d5148f7cfe96d2bf65d975a25a66b2ff2498f902
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2021-03-03 09:00:37 +0300

    Example Command

---
+++ src/commands/example.js
@@ -0,0 +1,16 @@
+module.exports = {
+  info: {
+    names: ["example", "example-2"], // Command Name(s)
+    description: "Example Description", // Command Description
+  },
+  config: {
+    enable: true, //true - false
+    maintenance: false, //true - false
+    permission: {
+      author: [] //https://discord.com/developers/docs/topics/permissions#permissions-bitwise-permission-flags
+    },
+  },
+  async run(client, message, args) {
+    // run is command
+  }
+};
