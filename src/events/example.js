ef906b5e8308af6c537bd115ea327c2a0452030d
Arda Engin Ebcim <66255272+ArdaEnginEbcim@users.noreply.github.com>
2021-03-03 09:07:14 +0300

    Example Event

---
+++ src/events/example.js
@@ -0,0 +1,12 @@
+module.exports = {
+  info: {
+    name: "message", // Event Name (message, ready, guildMemberAdd etc.)
+  },
+  config: {
+    enable: true, // true - false
+  },
+  once: false, // true - false
+  async run(client, message) {
+    // Event
+  }
+};
